784c486c23d56fe9803509232775657cb09e4983
francooparodi01 <francoparodi2001@gmail.com>
2024-11-14 15:10:13 -0300

    Agregando threads

--- cotizacion.py
+++ cotizacion.py
@@ -336,5 +336,4 @@ def echo_all(message):
 # Main
 if __name__ == '__main__':
     threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000}).start()
-    server_thread = threading.Thread(target=run_telegram_bot)
     bot.polling(none_stop=True)
